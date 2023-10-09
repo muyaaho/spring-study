55177a680a15ef7afb17a9c266fdd1d3aea8ef46
muyaaho <skanthsu715@gmail.com>
2023-10-10 00:11:50 +0900

    Refactor: add package

--- learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/helloworld/App02HelloWorldSpring.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/helloworld/App02HelloWorldSpring.java
@@ -1,4 +1,6 @@
-package com.in28minutes.learnspringframework;
+package com.in28minutes.learnspringframework.helloworld;
+
+import java.util.Arrays;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -7,26 +9,37 @@ public class App02HelloWorldSpring {
 	public static void main(String[] args) {
 		
 		// 1: Launch a Spring Context
-		
-		var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
-		
-		// 2: Configure the things that we want Spring to manage - 
-		// HelloWorldConfiguration - @Configuration
-		// name - @Bean
-		
-		// 3: Retrieving Beans managed by Spring
-		System.out.println(context.getBean("name"));
-		System.out.println(context.getBean("age"));
-		System.out.println(context.getBean("person"));
-		System.out.println(context.getBean("person2MethodCall"));
-		System.out.println(context.getBean("person3Parameters"));
-		System.out.println(context.getBean("address2"));
-		//System.out.println(context.getBean(Address.class));
-		/*
-		 * 에러 생기는 이유: Address.class는 spring bean을 불러오는데(getBean) address인 bean?이
-		 * address2, address3 둘 다 있어 어떤 bean을 가져올지 몰라서 생기는 에러
-		 */
-		
+		try(var context = new AnnotationConfigApplicationContext
+				(HelloWorldConfiguration.class)){
+			// 2: Configure the things that we want Spring to manage - 
+			// HelloWorldConfiguration - @Configuration
+			// name - @Bean
+			
+			// 3: Retrieving Beans managed by Spring
+			System.out.println(context.getBean("name"));
+			System.out.println(context.getBean("age"));
+			System.out.println(context.getBean("person"));
+			System.out.println(context.getBean("person2MethodCall"));
+			System.out.println(context.getBean("person3Parameters"));
+			System.out.println(context.getBean("address2"));
+			
+			System.out.println();
+			System.out.println(context.getBean(Person.class));
+			System.out.println(context.getBean(Address.class));
+			/*
+			 * 에러 생기는 이유: Address.class는 spring bean을 불러오는데(getBean) address인 bean?이
+			 * address2, address3 둘 다 있어 어떤 bean을 가져올지 몰라서 생기는 에러
+			 * 해결 1: @Primary - 하나를 기본으로 만들기
+			 * 해결 2: @Qualifier("이름") - 한정자 지정해주기
+			 */
+			
+			System.out.println();
+			System.out.println(context.getBean("person5Qualifier"));
+			
+			Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+
+		}
+			
 	}
 
 }
--- learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/helloworld/HelloWorldConfiguration.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/helloworld/HelloWorldConfiguration.java
@@ -1,7 +1,9 @@
-package com.in28minutes.learnspringframework;
+package com.in28minutes.learnspringframework.helloworld;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 // record: JDK 16에서 새로 추가된 기능
 // 게터, 세터 생성자 등을 만들지 않아도 됨 
@@ -36,12 +38,25 @@ public class HelloWorldConfiguration {
 		return new Person(name, age, address3);	 // 매개변수 호출
 	}
 	
+	@Bean
+	@Primary
+	public Person person4Parameters(String name, int age, Address address3) {	// name, age, address2
+		return new Person(name, age, address3);	 // 매개변수 호출
+	}
+	
+	@Bean
+	public Person person5Qualifier(String name, int age, @Qualifier("address3qualifier") Address address) {	// name, age, address2
+		return new Person(name, age, address);	 // 매개변수 호출
+	}
+	
 	@Bean(name = "address2")
+	@Primary
 	public Address address() {
 		return new Address("Baker Street", "London");
 	}
 	
 	@Bean(name = "address3")
+	@Qualifier("address3qualifier")
 	public Address address3() {
 		return new Address("Udu-dong", "Chuncheon");
 	}
